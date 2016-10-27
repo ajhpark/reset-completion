a72b66de946ee50cc324606b70bea7c601ce9163
Andrew Park <Andrew.Park@vch.ca>
2016-10-27 16:03:45 -0700

    delete choices module records

--- block_resetcompletion.php
+++ block_resetcompletion.php
@@ -18,9 +18,6 @@ class block_resetcompletion extends block_base {
         // Get course completion data
         $info = new completion_info($this->page->course);
 
-        // Get self-completion status
-        //$completion = $info->get_completion($USER->id, COMPLETION_CRITERIA_TYPE_SELF);
-
         // Check this user is enroled
         if (!$info->is_tracked_user($USER->id)) {
             $this->content->text = 'You are not enrolled';
@@ -29,21 +26,11 @@ class block_resetcompletion extends block_base {
 
         // Is course complete?
         if ($info->is_course_complete($USER->id)) {
-            $this->content->text = '';
+            $this->content->text = 'Click the link below to reset your completion data for this course. <br/><br/>WARNING: It will delete your completion data for this course. Only use this if you require recertification, or if you wish to completely retake the course from scratch.';
             $this->content->footer = '<br><a href="../blocks/resetcompletion/reset_user_completion.php?course=' . $this->page->course->id . '">';
             $this->content->footer .= 'Reset Completion' . '</a>';
             return $this->content;
         } 
-        // Check if the user has already marked themselves as complete
-        /*
-        else if ($completion->is_complete()) {
-            $this->content->text = '';
-            $this->content->footer = '<br><a href="../blocks/resetcompletion/reset_user_completion.php?course=' . $this->page->course->id . '">';
-            $this->content->footer .= 'Reset Completion </a>';
-            return $this->content;
-        // If user is not complete, or has not yet self completed
-         
-        } */
 
         else {
             $this->content->text = 'You have not completed the course yet';
--- reset_user_completion.php
+++ reset_user_completion.php
@@ -7,44 +7,39 @@
     $PAGE->set_context(context_system::instance());
     $courseid = optional_param('course', 0, PARAM_INT);
     $user = $USER->id;
-/*
-    $conn = mysql_connect($CFG->dbhost, $CFG->dbuser, $CFG->dbpass)
-        or die("Unable to connect to MySQL");
-    $selected = mysql_select_db($CFG->dbname, $conn) 
-        or die("Could not select Moodle db"); 
-    if ($courseid && $user) {
-        mysql_query('DELETE FROM mdl_course_modules_completion WHERE userid=' . $user . ' AND coursemoduleid IN (SELECT id FROM mdl_course_modules WHERE course=' . $courseid . ')');
-        mysql_query('DELETE FROM mdl_course_completions WHERE userid=' . $user . ' AND course=' . $courseid);
-        mysql_query('DELETE FROM mdl_course_completion_crit_compl WHERE userid=' . $user . ' AND course=' . $courseid);
-        redirect($CFG->wwwroot . '/course/view.php?id=' . $courseid);
-    } else {
-        echo "Course reset failed"; 
-        redirect($CFG->wwwroot . '/course/view.php?id=' . $courseid);
-    }
- */
-        global $DB;
-        $DB->delete_records_select('course_modules_completion',
-                'coursemoduleid IN (SELECT id FROM mdl_course_modules WHERE course=?) AND userid=?',
-                array($courseid, $user));
-        $DB->delete_records('course_completions', array('course' => $courseid, 'userid' => $user));
-        $DB->delete_records('course_completion_crit_compl', array('course' => $courseid, 'userid' => $user));
-        $DB->delete_records_select('scorm_scoes_track',
-                'scormid IN (SELECT id FROM mdl_scorm WHERE course=?) AND userid=?',
-                array($courseid, $user));
-        $orphanedattempts = $DB->get_records_sql_menu("
-            SELECT id, uniqueid
-              FROM {quiz_attempts}
-            WHERE userid=$user AND quiz IN (SELECT id FROM mdl_quiz WHERE course=$courseid)");
+    
+    global $DB;
+    
+    // Delete individual module completion records for current user
+    $DB->delete_records_select('course_modules_completion',
+            'coursemoduleid IN (SELECT id FROM mdl_course_modules WHERE course=?) AND userid=?',
+            array($courseid, $user));
+    // Delete course completion records
+    $DB->delete_records('course_completions', array('course' => $courseid, 'userid' => $user));
+    $DB->delete_records('course_completion_crit_compl', array('course' => $courseid, 'userid' => $user));
+    // Delete any user choices
+    $DB->delete_records_select('choice_answers',
+            'choiceid IN (SELECT id FROM mdl_choice WHERE course=?) AND userid=?',
+            array($courseid, $user));
+    // Delete SCORM data related to the user
+    $DB->delete_records_select('scorm_scoes_track',
+            'scormid IN (SELECT id FROM mdl_scorm WHERE course=?) AND userid=?',
+            array($courseid, $user));
 
-              
-        if ($orphanedattempts) {
-           foreach ($orphanedattempts as $attemptid => $usageid) {
-               question_engine::delete_questions_usage_by_activity($usageid);
-               $DB->delete_records('quiz_attempts', array('id' => $attemptid));
-           }
-        }
-        
-        cache::make('core', 'completion')->purge();
-        redirect($CFG->wwwroot . '/course/view.php?id=' . $courseid);
+    // Delete orphaned quiz attempts made by the user, after the completion record for the quiz has been deleted
+    $orphanedattempts = $DB->get_records_sql_menu("
+        SELECT id, uniqueid
+          FROM {quiz_attempts}
+        WHERE userid=$user AND quiz IN (SELECT id FROM mdl_quiz WHERE course=$courseid)");
+          
+    if ($orphanedattempts) {
+       foreach ($orphanedattempts as $attemptid => $usageid) {
+           question_engine::delete_questions_usage_by_activity($usageid);
+           $DB->delete_records('quiz_attempts', array('id' => $attemptid));
+       }
+    }
+    
+    cache::make('core', 'completion')->purge();
+    redirect($CFG->wwwroot . '/course/view.php?id=' . $courseid);
 
 ?>
